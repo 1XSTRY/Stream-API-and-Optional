374ad97a3c99a6630379abe4e55b980b6324b3e0
1XSTRY <kosogornov99@gmail.com>
2024-01-07 16:32:46 +0500

    initial commit

---
+++ src/test/java/sky/pro/Stream/API/and/Optional/StreamApiAndOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.Stream.API.and.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiAndOptionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
